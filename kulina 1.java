f9a0b49356ed694d97922939aa85893e3573466b
Unknown <andreas.aldito.08@gmail.com>
2018-07-13 10:29:18 +0700

    All in java language

---
+++ kulina 1.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+
+
+
+public class MyClass 
+{
+    
+	public static void main(String args[]) 
+    
+	{
+        
+		Scanner show = new Scanner(System.in);
+        
+		System.out.print("Input angka = ");
+		
+		int input = show.nextInt();
+		
+		
+		System.out.println(find(input));
+        
+    
+	}
+    
+    
+
+	public static String find(int inputX)
+    
+	{
+        
+		int x=0;
+        
+		boolean result = false;
+        
+		double checkRoot = Math.sqrt(x);
+    
+        
+		if (Math.floor(checkRoot)==checkRoot)
+        
+		{
+                
+			result = false;
+        
+		} 
+        
+        
+		
+		else if( ((x%2!=0 && x%3!=0 && x%5!= 0 && x%7!=0) || (x==2 || x==3 || x==5 || x==7)))
+        
+		{
+           
+			result = true;
+        
+		}
+        
+        
+
+		if(result)
+        
+		{
+            
+			System.out.println("PRIMA");
+        
+		} 
+        
+		
+		else
+        
+		{
+            
+			System.out.println("BUKAN PRIMA");
+        
+		}
+        
+        
+		
+	return "";
+    
+	}
+    
+
+}
---
+++ kulina 2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+
+
+
+public class MyClass 
+{
+    
+	public static void main(String args[]) 
+    
+	{
+        
+        
+		Scanner show = new Scanner(System.in);
+
+		System.out.print("Input angka = ");
+
+		int input = show.nextInt();
+
+		long fibo[] = new long[input];
+
+		
+fibo[0] = 0;
+		
+		fibo[1] = 1;
+		
+		
+
+		for(int i = 2; i < input; i++) 
+		{
+			
+			fibo[i] = fibo[i-1] + fibo[i-2];
+		
+		}
+		
+		
+
+		for (int i = 0; i < input; i++) 
+		{
+			
+			System.out.print(fibo[i] +  " ");
+		
+		}
+        
+        
+    
+	}
+
+}
